1ccb234e290baa7254cf8264952dfbf753eda53d
duytanisme <duytanisme02@gmail.com>
2025-01-27 00:30:51 +0700

    update main with more error handling

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import re
 import threading
 import time
 from logging.config import dictConfig
-from typing import Dict
+from typing import Dict, List, Union, Any
 
 import TikTokLive.client.errors
 import pyttsx3
@@ -21,11 +21,13 @@ load_dotenv()
 USER_ID = os.getenv("USER_ID")
 
 FINE_STATUS = {
-    "PENDING": "Chưa xử phạt", "COMPLETE": "Đã xử phạt",
+    "PENDING": "Chưa xử phạt",
+    "COMPLETE": "Đã xử phạt",
 }
 
 PROPERTIES = {
-    "ACTION": "Hành vi vi phạm", "STATUS": "Trạng thái",
+    "ACTION": "Hành vi vi phạm",
+    "STATUS": "Trạng thái",
     "TIME": "Thời gian vi phạm",
 }
 
@@ -37,41 +39,58 @@ with open(config_file_path) as json_file:
 logger = logging.getLogger("default")
 
 
-def say_message(message_to_say):
+def say_message(message_to_say: str):
     """
     Say a message in Vietnamese.
     :param message_to_say: Message to say.
     :type message_to_say: str
     """
     engine = pyttsx3.init()
-    voices = engine.getProperty('voices')
+    voices = engine.getProperty("voices")
     # noinspection PyUnresolvedReferences
-    engine.setProperty('voice', voices[1].id)
-    engine.setProperty('rate', 130)
+    engine.setProperty("voice", voices[1].id)
+    engine.setProperty("rate", 130)
     engine.say(message_to_say)
     engine.runAndWait()
 
 
-def get_plate_data(plate_str: str) -> Dict[str, str]:
+def get_plate_data(plate_str: str) -> Union[Dict[str, Union[str, Any]], Any]:
     ua = UserAgent()
     headers = {
-        'accept': '*/*', 'content-type': 'application/json',
-        'origin': 'https://checkphatnguoi.vn',
-        'referer': 'https://checkphatnguoi.vn/', 'user-agent': ua.random,
+        "accept": "*/*",
+        "content-type": "application/json",
+        "origin": "https://checkphatnguoi.vn",
+        "referer": "https://checkphatnguoi.vn/",
+        "user-agent": ua.random,
     }
-    json_data = {'bienso': plate_str}
+    json_data = {
+        "bienso": plate_str,
+    }
 
     try:
-        response = requests.post('https://api.checkphatnguoi.vn/phatnguoi',
-            headers=headers, json=json_data
+        response = requests.post(
+            "https://api.checkphatnguoi.vn/phatnguoi",
+            headers=headers,
+            json=json_data,
+            timeout=20,
         )
         return response.json()
+    except requests.exceptions.Timeout:
+        logger.error(f"Timeout")
+        return None
+    except requests.exceptions.RequestException:
+        logger.error(f"RequestException")
+        return None
     except Exception as e:
         logger.error(f"Error: {e}")
-        return {}
+        return None
 
 
-def handle_fault(plate_data):
+def handle_fault(
+    plate_data: Union[Dict[str, Union[List[Dict[str, str]], Any]], Any]
+) -> None:
+    if plate_data is None:
+        return
     faults = plate_data.get("data", [])
     if 0 < len(faults) < 10:
         for fault in faults:
@@ -89,37 +108,40 @@ def process_plate_queue():
             continue
 
         plate_number = plate["plate_number"]
-        plate_owner = plate['plate_owner']
+        plate_owner = plate["plate_owner"]
         message = f"Đang kiểm tra biển {plate_number} của {plate_owner}..."
         logger.info(message)
         say_message(message)
 
         plate_data = get_plate_data(plate_number)
-        if plate_data.get("status") == 1:
-            cxp = plate_data.get("data_info", {}).get("chuaxuphat", 0)
-            dxp = plate_data.get("data_info", {}).get("daxuphat", 0)
-            sum_fault = int(cxp) + int(dxp)
-            message = f"Biển số {plate_number} - Vi phạm {sum_fault} lỗi | Chưa xử phạt: {cxp} | Đã xử phạt: {dxp}"
-            logger.info(message)
-            say_message(message)
-            handle_fault(plate_data)
+        if plate_data is not None:
+            if plate_data.get("status") == 1:
+                cxp = plate_data.get("data_info", {}).get("chuaxuphat", 0)
+                dxp = plate_data.get("data_info", {}).get("daxuphat", 0)
+                sum_fault = int(cxp) + int(dxp)
+                message = f"Biển số {plate_number} - Vi phạm {sum_fault} lỗi | Chưa xử phạt: {cxp} | Đã xử phạt: {dxp}"
+                logger.info(message)
+                say_message(message)
+                handle_fault(plate_data)
 
-        elif plate_data.get("status") == 2:
-            message = f"Biển số {plate_number} - Không có lỗi vi phạm"
-            logger.info(message)
-            say_message(message)
+            elif plate_data.get("status") == 2:
+                message = f"Biển số {plate_number} - Không có lỗi vi phạm"
+                logger.info(message)
+                say_message(message)
 
-        elif plate_data.get("status") == 3:
-            message = f"Biển số {plate_number} - {plate_data.get('msg')}"
-            logger.info(message)
-            say_message(message)
+            elif plate_data.get("status") == 3:
+                message = f"Biển số {plate_number} - {plate_data.get('msg')}"
+                logger.info(message)
+                say_message(message)
+        else:
+            logger.warning(f"No data for {plate_number}")
 
         plate_queue.task_done()
         logger.info("-" * 20)
 
 
-def get_match_plate(input_message):
-    cleaned_message = re.sub(r'[^a-zA-Z0-9]', '', input_message.lower())
+def get_match_plate(input_message: str):
+    cleaned_message = re.sub(r"[^a-zA-Z0-9]", "", input_message.lower())
     pattern = r"\b\d{2}[A-Za-z]\d?-?\d{4,6}\b"
     return re.search(pattern, cleaned_message)
 
@@ -142,8 +164,10 @@ async def on_comment(event: CommentEvent):
     match_plate = get_match_plate(comment)
     if match_plate:
         plate_number = match_plate.group()
-        plate_queue.put(
-            {"plate_owner": event.user.nickname, "plate_number": plate_number}
+        plate_queue.put({
+            "plate_owner": event.user.nickname,
+            "plate_number": plate_number,
+        }
         )
 
 
@@ -157,6 +181,9 @@ if __name__ == "__main__":
         except TikTokLive.client.errors.UserNotFoundError:
             logger.error("User not found")
             break
-        except TikTokLive.client.errors as gne:
+        except KeyboardInterrupt:
+            logger.info("Exiting")
+            break
+        except Exception as gne:
             logger.error(f"TikTok error: {gne}")
             break
